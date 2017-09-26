d382929f02c0dce770a46bde6d1cef13fc14e205
Eduardo Zimermam <eduardozimerman@live.com>
2017-09-26 00:52:28 -0300

    Método de Gauss pronto.

--- gauss.c
+++ gauss.c
@@ -2,34 +2,70 @@
 
 int main() {
 
-	float r = 0;
 	int dim = 3, i, j, y;
-	float m[dim][dim];
+	float m[dim][dim], r;
 	double u[dim][dim];
+	double l[dim][dim];
 
+//Zera as martizes para garantir que não haverá lixo de memória.
 	for(i = 0; i < dim; i++){
 		for(j = 0; j < dim; j++){
-			scanf("%f", &m[i][j]);
+			m[i][j] = 0;
+			u[i][j] = 0;
+			l[i][j] = 0;
 		}
 	}
 
-	for(j = 0; j < dim; j++){
-		u[0][j] = m[0][j];
-	}
+//Faz a leitura da matriz de exemplo.
+		for(i = 0; i < dim; i++){
+			for(j = 0; j < dim; j++){
+				scanf("%f", &m[i][j]);
+			}
+		}
 
-	for(i = 1; i < dim; i++){
-		r = m[i][0] / m[i - 1][0];
+//Seta um espelho da matriz m em u para que todas as operações sejam efetuadas em u.
+	for(i = 0; i < dim; i++){
 		for(j = 0; j < dim; j++){
-			u[i][j] = (r * ((-1) * m[i - 1][j])) + m[i][j];
+			u[i][j] = m[i][j];
 		}
 	}
 
+
+//Aplicação do método de Gauss através do espelho da matriz m sobre a matriz u.
+	for(y = 0; y < dim; y++){
+		for(i = y + 1; i < dim; i++){
+			r = u[i][y] / u[y][y];
+			l[i][y] = r;
+			for(j = y; j < dim; j++){
+				u[i][j] = (r * ((-1) * u[y][j])) + u[i][j];
+			}
+		}
+	}
+
+//Normalização da matriz l para que sua diagonal principal receba o valor 1.
+	for(i = 0; i < dim; i++){
+		l[i][i] = 1;
+	}
+
+
+//Demonstração das matrizes através do printf.
+ 	printf("\nMATRIZ U:\n");
 	for(i = 0; i < dim; i++){
 		for(j = 0; j < dim; j++){
-			printf("%.2lf ", u[i][j]);
+			printf("%.17g ", u[i][j]);
 		}
 		printf("\n");
 	}
+
+	printf("\nMATRIZ L:\n");
+	for(i = 0; i < dim; i++){
+		for(j = 0; j < dim; j++){
+			printf("%.17g ", l[i][j]);
+		}
+		printf("\n");
+	}
+
+
 
 	return 0;
 }
